dabd9bc4d7ba525ca918934eb1c4cda784d7a4f3
kevin <kevinpernia29@gmail.com>
2025-08-19 17:43:07 -0500

    Allow All origins

--- src/main/java/com/logisticapp/backend_logistic_app/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/logisticapp/backend_logistic_app/infrastructure/config/SecurityConfig.java
@@ -69,10 +69,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
